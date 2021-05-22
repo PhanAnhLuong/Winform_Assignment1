42df83c32525696405da9c6cedffcad6a9fccac7
ADMIN\Admin <anh.lp.a1908g03@aptechlearning.edu.vn>
2021-05-22 21:59:25 +0700

    BTVN 22/5/2021

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDataGridView();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private DataTable dataTable = new DataTable();
+        void LoadDataGridView()
+        {
+            dataTable.Columns.Add("Id", typeof(int));
+            dataTable.Columns.Add("First Name", typeof(String));
+            dataTable.Columns.Add("Last Name", typeof(String));
+            dataTable.Columns.Add("Name", typeof(String));
+
+            dataGridView1.DataSource = dataTable;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataTable.Rows.Add(textBox4.Text, textBox1.Text, textBox2.Text, textBox3);
+            dataGridView1.DataSource = dataTable;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
